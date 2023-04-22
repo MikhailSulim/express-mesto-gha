8f529c21dbbf664dfb962e6953f2e84034f53cdb
Mikhail Sulim <mika-1984@yandex.ru>
2023-04-22 18:16:08 +0300

    feature: создать схему и модель для карточки и пользователя

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const app = express();
-const { PORT = 3003 } = process.env;
-console.log(process.env);
+const mongoose = require("mongoose");
+const { PORT = 3000 } = process.env;
+// console.log(process.env);
+
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+// app.get("/users", (req, res) => {
+//   res.send("fdgdfg");
+// });
+
+// app.get("/users/:id", (req, res) => {
+//   const { id } = req.params;
+//   console.log(req.params, typeof id);
+//   res.send(req.params);
+// });
+
+// app.post("/users/", (req, res) => {
+//   console.log(req);
+//   res.send("dsfsdf");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
---
+++ models/card.js
@@ -0,0 +1,38 @@
+// схема и модель данных о карточке для записи в БД
+const mongoose = require("mongoose");
+
+// создаём схему
+const cardSchema = new mongoose.Schema({
+  name: {
+    // имя карточки, строка от 2 до 30 символов, обязательное поле
+    type: String,
+    required: [true, "необходимо задать имя карточки"],
+    minlength: [2, "длина имени карточки менее двух символов"],
+    maxlength: [30, "длина имени карточки более 30 символа"],
+  },
+  link: {
+    // ссылка на картинку, строка, обязательно поле
+    type: String,
+    required: [true, "необходимо задать ссылку на картинку"],
+  },
+  owner: {
+    // ссылка на модель автора карточки, тип ObjectId, обязательное поле
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: [true, "необходимо задать ссылку на модель автора карточки"],
+  },
+  likes: {
+    //список лайкнувших пост пользователей, массив ObjectId, по умолчанию — пустой массив (поле default)
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "user",
+    default: [],
+  },
+  createdAt: {
+    // дата создания, тип Date, значение по умолчанию Date.now
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// создаём модель
+module.exports = mongoose.model("card", cardSchema);
